12854885abb5ef052b9499205ca5bc8502c6b6ad
Gari-29 <112106520+Gari-29@users.noreply.github.com>
2022-09-29 19:42:00 +0530

    Add files via upload
    Reverse string assignment

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include<string.h>
+
+main()
+{
+    char Str[10];
+    int len,i;
+
+    printf("\n enter a string: ");
+    gets(Str);
+
+    len = strlen(Str);
+
+    printf("\n reverse string: ");
+    for(i= len-1; i>=0; i--)
+    {
+        printf("%c",Str[i]);
+    }
+    getch();
+}
+
+
+
+
